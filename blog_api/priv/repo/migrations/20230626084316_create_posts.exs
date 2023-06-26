ce10acf68c1a1d4d98b2bae887803a23aabc5e82
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-26 18:01:11 +0900

    ✨ feat(migrations): add migrations to create accounts and posts tables with necessary columns and constraints

--- blog_api/priv/repo/migrations/20230626084316_create_posts.exs
+++ blog_api/priv/repo/migrations/20230626084316_create_posts.exs
@@ -7,6 +7,7 @@ defmodule BlogApi.Repo.Migrations.CreatePosts do
       add :body, :text
       add :type, :integer, null: false
       add :submit_datetime, :utc_datetime
+      add :account_id, references(:accounts, on_delete: :delete_all), null: false
 
       timestamps(type: :utc_datetime)
     end

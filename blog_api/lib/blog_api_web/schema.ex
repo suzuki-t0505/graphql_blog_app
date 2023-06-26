781ca0444581a81c55375e0dbad2ae81d0a374a5
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-26 16:23:55 +0900

    🚀 feat(schema.ex): add datetime scalar to serialize and parse datetime values in ISO format 🚀 feat(schema.ex): add post object with fields id, title, body, type, and submit_datetime 🚀 feat(schema.ex): add posts query to retrieve a list of posts with their respective fields

--- blog_api/lib/blog_api_web/schema.ex
+++ blog_api/lib/blog_api_web/schema.ex
@@ -1,7 +1,33 @@
 defmodule BlogApiWeb.Schema do
   use Absinthe.Schema
 
-  # query do
+  scalar :datetime do
+    description "DateTime for 2022-03-01 00:00:00"
+    parse &parse_datetime/1
+    serialize &Calendar.strftime(&1, "%c")
+  end
 
-  # end
+  object :post do
+    field :id, :id
+    field :title, :string
+    field :body, :string
+    field :type, :integer
+    field :submit_datetime, :datetime
+  end
+
+  query do
+    field :posts, list_of(:post) do
+      resolve(fn _, _, _ -> {:ok, BlogApi.Posts.list_posts} end)
+    end
+  end
+
+
+  defp parse_datetime(data) do
+    with {:ok, naive_datetime} <- Timex.parse(data, "{ISOdate} {ISOtime}"),
+      datetime <- Timex.to_datetime(naive_datetime) do
+        {:ok, datetime}
+    else
+      _ -> {:error, data}
+    end
+  end
 end

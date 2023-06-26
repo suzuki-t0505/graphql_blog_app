6de1332fbdee51b163cde711341759225a40200c
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-26 18:11:04 +0900

    📝 docs(accounts.ex): add documentation to Accounts context 🔒 chore(accounts.ex): remove unused import and add alias to Repo 🔒 chore(accounts.ex): remove unused import and add alias to Account, AccountToken and AccountNotifier 🔒 chore(accounts.ex): remove unused function get_account_by_email_and_password 🔒 chore(accounts.ex): remove unused function get_account_by_email 🔒 chore(accounts.ex): remove unused function get_account 🔒 chore(accounts.ex): remove unused function change_account_registration 🔒 chore(accounts.ex): remove unused function change_account_email 🔒 chore(accounts.ex): remove unused function apply_account_email 🔒 chore(accounts.ex): remove unused function update_account_email 🔒 chore(accounts.ex): remove unused function account_email_multi 🔒 chore(accounts.ex): remove unused function deliver_account_update_email_instructions 🔒 chore(accounts.ex): remove unused function change_account_password 🔒 chore(accounts.ex): remove unused function update_account_password 🔒 chore(accounts.ex): remove unused function generate_account_session_token 🔒 chore(accounts.ex): remove unused function get_account_by_session_token 🔒 chore(accounts.ex): remove unused function delete_account_session_token 🔒 chore(accounts.ex): remove unused function confirm_account 🔒 chore(accounts.ex): remove unused function confirm_account_multi 🔒 chore(accounts.ex): remove unused function deliver_account_confirmation_instructions 🔒 chore(accounts.ex): remove unused function get_account_by_reset_password_token 🔒 chore(accounts.ex): remove unused function reset_account_password ✨ feat(seeds.exs): add seed data for accounts 🔒 chore(seeds.exs): remove unused variable _user02 and _user03 🔒 chore(seeds.exs): remove unused import and add alias to Account 🔒 chore(seeds.exs): remove unused import and add alias to Repo 🔒 chore(seeds.exs): remove unused function call and add account_id to posts insert

---
+++ blog_api/lib/blog_api/accounts.ex
@@ -0,0 +1,199 @@
+defmodule BlogApi.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  import Ecto.Query, warn: false
+  alias BlogApi.Repo
+
+  alias BlogApi.Accounts.{Account, AccountToken, AccountNotifier}
+
+  ## Database getters
+  def get_account_by_email(email) when is_binary(email) do
+    Repo.get_by(Account, email: email)
+  end
+
+  def get_account_by_email_and_password(email, password)
+      when is_binary(email) and is_binary(password) do
+    account = Repo.get_by(Account, email: email)
+    if Account.valid_password?(account, password), do: account
+  end
+
+  def get_account!(id), do: Repo.get!(Account, id)
+
+  def get_account(id), do: Repo.get(Account, id)
+
+
+  ## Account registration
+
+  def register_account(attrs) do
+    %Account{}
+    |> Account.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def change_account_registration(%Account{} = account, attrs \\ %{}) do
+    Account.registration_changeset(account, attrs, hash_password: false, validate_email: false)
+  end
+
+  ## Settings
+
+  def change_account_email(account, attrs \\ %{}) do
+    Account.email_changeset(account, attrs, validate_email: false)
+  end
+
+  def apply_account_email(account, password, attrs) do
+    account
+    |> Account.email_changeset(attrs)
+    |> Account.validate_current_password(password)
+    |> Ecto.Changeset.apply_action(:update)
+  end
+
+  def update_account_email(account, token) do
+    context = "change:#{account.email}"
+
+    with {:ok, query} <- AccountToken.verify_change_email_token_query(token, context),
+        %AccountToken{sent_to: email} <- Repo.one(query),
+        {:ok, _} <- Repo.transaction(account_email_multi(account, email, context)) do
+      :ok
+    else
+      _ -> :error
+    end
+  end
+
+  defp account_email_multi(account, email, context) do
+    changeset =
+      account
+      |> Account.email_changeset(%{email: email})
+      |> Account.confirm_changeset()
+
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:account, changeset)
+    |> Ecto.Multi.delete_all(:tokens, AccountToken.account_and_contexts_query(account, [context]))
+  end
+
+  def deliver_account_update_email_instructions(
+        %Account{} = account,
+        current_email,
+        update_email_url_fun
+      )
+      when is_function(update_email_url_fun, 1) do
+    {encoded_token, account_token} =
+      AccountToken.build_email_token(account, "change:#{current_email}")
+
+    Repo.insert!(account_token)
+
+    AccountNotifier.deliver_update_email_instructions(
+      account,
+      update_email_url_fun.(encoded_token)
+    )
+  end
+
+  def change_account_password(account, attrs \\ %{}) do
+    Account.password_changeset(account, attrs, hash_password: false)
+  end
+
+  def update_account_password(account, password, attrs) do
+    changeset =
+      account
+      |> Account.password_changeset(attrs)
+      |> Account.validate_current_password(password)
+
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:account, changeset)
+    |> Ecto.Multi.delete_all(:tokens, AccountToken.account_and_contexts_query(account, :all))
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{account: account}} -> {:ok, account}
+      {:error, :account, changeset, _} -> {:error, changeset}
+    end
+  end
+
+  ## Session
+
+  def generate_account_session_token(account) do
+    {token, account_token} = AccountToken.build_session_token(account)
+    Repo.insert!(account_token)
+    token
+  end
+
+  def get_account_by_session_token(token) do
+    {:ok, query} = AccountToken.verify_session_token_query(token)
+    Repo.one(query)
+  end
+
+  def delete_account_session_token(token) do
+    Repo.delete_all(AccountToken.token_and_context_query(token, "session"))
+    :ok
+  end
+
+  ## Confirmation
+
+  def deliver_account_confirmation_instructions(%Account{} = account, confirmation_url_fun)
+      when is_function(confirmation_url_fun, 1) do
+    if account.confirmed_at do
+      {:error, :already_confirmed}
+    else
+      {encoded_token, account_token} = AccountToken.build_email_token(account, "confirm")
+      Repo.insert!(account_token)
+
+      AccountNotifier.deliver_confirmation_instructions(
+        account,
+        confirmation_url_fun.(encoded_token)
+      )
+    end
+  end
+
+  def confirm_account(token) do
+    with {:ok, query} <- AccountToken.verify_email_token_query(token, "confirm"),
+        %Account{} = account <- Repo.one(query),
+        {:ok, %{account: account}} <- Repo.transaction(confirm_account_multi(account)) do
+      {:ok, account}
+    else
+      _ -> :error
+    end
+  end
+
+  defp confirm_account_multi(account) do
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:account, Account.confirm_changeset(account))
+    |> Ecto.Multi.delete_all(
+      :tokens,
+      AccountToken.account_and_contexts_query(account, ["confirm"])
+    )
+  end
+
+  ## Reset password
+
+  def deliver_account_reset_password_instructions(%Account{} = account, reset_password_url_fun)
+      when is_function(reset_password_url_fun, 1) do
+    {encoded_token, account_token} = AccountToken.build_email_token(account, "reset_password")
+    Repo.insert!(account_token)
+
+    AccountNotifier.deliver_reset_password_instructions(
+      account,
+      reset_password_url_fun.(encoded_token)
+    )
+  end
+
+  def get_account_by_reset_password_token(token) do
+    with {:ok, query} <- AccountToken.verify_email_token_query(token, "reset_password"),
+        %Account{} = account <- Repo.one(query) do
+      account
+    else
+      _ -> nil
+    end
+  end
+
+  def reset_account_password(account, attrs) do
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:account, Account.password_changeset(account, attrs))
+    |> Ecto.Multi.delete_all(:tokens, AccountToken.account_and_contexts_query(account, :all))
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{account: account}} -> {:ok, account}
+      {:error, :account, changeset, _} -> {:error, changeset}
+    end
+end
+
+end
--- blog_api/priv/repo/seeds.exs
+++ blog_api/priv/repo/seeds.exs
@@ -1,5 +1,17 @@
 alias BlogApi.Repo
 alias BlogApi.Posts.Post
+alias BlogApi.Accounts.Account
+
+[user01, _user02, _user03] =
+  Enum.map(~w(user01 user02 user03), fn name ->
+    Repo.insert!(
+      %Account{
+        email: name <> "@sample.com",
+        hashed_password: Bcrypt.hash_pwd_salt(name <> "999"),
+        confirmed_at: DateTime.truncate(DateTime.utc_now(), :second)
+      }
+    )
+  end)
 
 posts = [
   %Post{
@@ -12,12 +24,14 @@ posts = [
     title: "投稿02",
     body: "これは投稿02です。\nこれは投稿02です。",
     type: 1,
-    submit_datetime: DateTime.truncate(DateTime.utc_now(), :second)  },
+    submit_datetime: DateTime.truncate(DateTime.utc_now(), :second)
+  },
   %Post{
     title: "投稿03",
     body: "これは投稿03です。\nこれは投稿03です。",
     type: 1,
-    submit_datetime: DateTime.truncate(DateTime.utc_now(), :second)  }
+    submit_datetime: DateTime.truncate(DateTime.utc_now(), :second)
+  }
 ]
 
-Enum.each(posts, fn post -> Repo.insert!(post) end)
+Enum.each(posts, fn post -> Repo.insert!(%{post | account_id: user01.id}) end)

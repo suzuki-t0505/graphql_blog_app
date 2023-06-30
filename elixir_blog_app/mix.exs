b4cea80a5ce77205f3d942332ebdbe9f9a6e97fc
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-30 12:09:06 +0900

    📦 chore(mix.exs): add absinthe_client, ecto, and phoenix_ecto dependencies for GraphQL and database support

--- elixir_blog_app/mix.exs
+++ elixir_blog_app/mix.exs
@@ -46,7 +46,10 @@ defmodule ElixirBlogApp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe_client, "~> 0.1.0"},
+      {:ecto, "~> 3.10.2"},
+      {:phoenix_ecto, "~> 4.0"}
     ]
   end
 

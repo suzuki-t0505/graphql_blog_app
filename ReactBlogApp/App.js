6c6fdd97088cd0db94ac881821513d4f2bf3c569
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-07-10 16:24:47 +0900

    🔥 refactor(App.js): remove commented out code for WebSocketLink and split link 📝 docs(App.js): add ApolloClient configuration with authedHttpLink and InMemoryCache

--- ReactBlogApp/App.js
+++ ReactBlogApp/App.js
@@ -43,28 +43,6 @@ export default function App() {
     authedHttpLink
   );
 
-  // const wsLink = new WebSocketLink({
-  //   uri: 'ws://localhost:4000/socket/websocket',
-  //   options: {
-  //     reconnect: true,
-  //     connectionParams: {
-  //       authToken: authToken
-  //     }
-  //   }
-  // });
-
-  // const link = split(
-  //   ({query}) => {
-  //     const { kind, operation } = getMainDefinition(query);
-  //     return (
-  //       kind === 'OperationDefinition' &&
-  //       operation === 'subscription'
-  //     );
-  //   },
-  //   wsLink,
-  //   authLink.concat(httpLink)
-  // );
-
   const client = useCallback(new ApolloClient({
     link: link,
     cache: new InMemoryCache()
